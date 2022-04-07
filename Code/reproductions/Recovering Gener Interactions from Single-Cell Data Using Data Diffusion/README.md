22d56e6296a9679b1fa0fe3cd55da4e4915b455d
jpickard1 <jpic@umich.edu>
2022-04-07 12:45:12 -0400

    Pulled original MAGIC code. I investigated how they compute the optimal t based on their method and generated a plot that shows the rank and norm of the imputed data as t increases

---
+++ Code/reproductions/Recovering Gener Interactions from Single-Cell Data Using Data Diffusion/README.md
@@ -0,0 +1,9 @@
+# ![Recovering Gener Interactions from Single-Cell Data Using Data Diffusion](https://www.sciencedirect.com/science/article/pii/S0092867418307244)
+
+Auth: Joshua Pickard jpic@umich.edu
+
+Created: April 7, 2022
+
+---
+
+This directory contains code to run the Markov Affinitiy based Graph Imputation of Cells (MAGIC) algorithm. The alorithm was developed by Smita Krishnaswamy and her lab. The majority of this code was downloaded from the Matlab directory at this git repository: https://github.com/KrishnaswamyLab/MAGIC
--- Code/reproductions/Recovering Gener Interactions from Single-Cell Data Using Data Diffusion/demo_magic.m
+++ Code/reproductions/Recovering Gener Interactions from Single-Cell Data Using Data Diffusion/demo_magic.m
@@ -1,5 +1,5 @@
 %% D
-num_cells = 20;
+num_cells = 200;
 num_genes = 500;
 D = rand(num_cells, num_genes);
 
@@ -7,6 +7,14 @@ known = randi([0 9], num_cells, num_genes);
 known = (known > 0);
 D_known = D;
 D_known(~known) = 0;
+
+% Compute optimal t
+%compute_optimal_t(D);
+
+% Run magic
+run_magic(D);
+
+saveas(gcf, 'Optimal t with MAGIC Rank and Norms.png')
 
 %% Dist
 dist = pdist(D);
@@ -29,6 +37,7 @@ end
 max_t = 30;
 imputed_measured_norms = zeros(max_t, 1);
 imputed_rank = zeros(max_t, 1);
+R_sq = zeros(max_t, 1); % This is the method implemented in the paper
 for t=1:max_t
     if t ~= 1
         M_exp = M_exp * M;
@@ -43,7 +52,7 @@ for t=1:max_t
     D_imputed_known(~known) = 0;
     imputed_measured_norm = norm(D_imputed_known - D_known, 'fro');
     imputed_measured_norms(t) = imputed_measured_norm;
-
+    % Rsq
 end
 figure;
 subplot(2,1,1);
