e771bf16317ec0e5492003174c1e7b322526ffcb
JPickard1 <jpic@umich.edu>
2023-03-13 21:54:51 -0400

    DATA: modified how USAir network is processed and stored so that the vertex names can be loaded as well.

--- Code/data processing/USAir.m
+++ Code/data processing/USAir.m
@@ -2,8 +2,26 @@
 % pairs file is a file with 3 columns: node i, node j, and the edge
 % connecting them
 %
-% file_path = 'C:\Users\picka\Documents\my_projects\DBTM\Main\Data\USAir\edge_pairs.txt'
-% 
+% Auth: Joshua Pickard
+%       jpic@umich.edu
+% Date: Unknown
+% Mod:  March 10, 2023
+
+%% Construct Vertex Name vector
+clear;
+file_path = 'C:\Users\picka\Documents\my_projects\DBTM\Main\Data\USAir\raw_download.txt';
+in = tdfread(file_path, '\t');
+data = in.x0x2AVertices_332;
+airports = cell(332,1);
+for i=1:332
+    r = data(i,:);
+    r = split(r,'"');
+    airports{i} = r{2};
+end
+save C:\Users\picka\Documents\my_projects\DBTM\Main\Data\USAir\nodeNames.mat airports
+
+%% Construct Adjacency Matrix
+file_path = 'C:\Users\picka\Documents\my_projects\DBTM\Main\Data\USAir\edge_pairs.txt';
 in = tdfread(file_path, '\t');
 data = in.x_v_v_e;
 v = max([data(:,1); data(:,2)]);
