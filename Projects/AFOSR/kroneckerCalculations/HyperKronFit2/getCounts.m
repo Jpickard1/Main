c1db49835fde7e7153b92a3916342b5144ff60d7
Joshua Pickard <jpic@umich.edu>
2023-06-19 15:10:46 -0400

    mod to work on ndArrays

--- Projects/AFOSR/kroneckerCalculations/HyperKronFit2/getCounts.m
+++ Projects/AFOSR/kroneckerCalculations/HyperKronFit2/getCounts.m
@@ -7,17 +7,21 @@ kronExp = log(n) / log(n0);
 
 % Count the number of times an entry of theta is used
 count = zeros(size(theta));
-% c = cell(kronExp, 1);
-% for i=1:length(idxs)
-%     c{:} = kIdxs(i,:) = kronIndices(idxs(i), n, n0);
-% 
-% end
-
-for i=1:kronExp
-    i1 = mod(floor((idxs(1) - 1)/ n0^(i-1)), n0) + 1;
-    i2 = mod(floor((idxs(2) - 1)/ n0^(i-1)), n0) + 1;
-    count(i1, i2) = count(i1, i2) + 1;
+c = cell(kronExp, 1);
+kIdx = zeros(kronExp, length(idxs));
+for i=1:length(idxs)
+    kIdxs(:,i) = kronIndices(idxs(i), n, n0);
 end
+for i=1:kronExp
+    c = num2cell(kIdxs(i,:));
+    count(c{:}) = count(c{:}) + 1;
+end
+
+% for i=1:kronExp
+%     i1 = mod(floor((idxs(1) - 1)/ n0^(i-1)), n0) + 1;
+%     i2 = mod(floor((idxs(2) - 1)/ n0^(i-1)), n0) + 1;
+%     count(i1, i2) = count(i1, i2) + 1;
+% end
 
 end
 
