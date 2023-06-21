84146612b198b1e412871f929fdb248e66f722ea
Joshua <jpic@umich.edu>
2023-06-21 09:28:31 -0400

    changes made for GL deployment

--- Projects/KroneckerHypergraphPaper/3CAnalysis/glHG2theta.sh
+++ Projects/KroneckerHypergraphPaper/3CAnalysis/glHG2theta.sh
@@ -7,13 +7,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=50GB
-#SBATCH --time=0-01:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --account=indikar0
 #SBATCH --partition=standard
-# SBATCH --array=0:39
+#SBATCH --array=0-29
 #SBATCH --output=/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/3CAnalysis/HG2theta/%x-%j.log
 
 
 module load matlab
-matlab -nodisplay -r "driverHG2theta('worker',1,'epsilon',0.01,'system','GL', 'n0', 2, 'filePath', '/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/3CAnalysis/HipHop2HG/', 'firstPermItrs', 10000, 'gradSamples', 100000, 'maxItrs', 75, 'learningRate', 1e-8', 'outputPath', '/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/3CAnalysis/HG2theta/');"
+matlab -nodisplay -r "driverHG2theta('worker',$SLURM_ARRAY_TASK_ID,'epsilon',0.01,'system','GL', 'n0', 2, 'filePath', '/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/3CAnalysis/HipHop2HG/', 'firstPermItrs', 10000, 'gradSamples', 100000, 'maxItrs', 75, 'learningRate', 1e-9', 'outputPath', '/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/3CAnalysis/HG2theta/');"
 
