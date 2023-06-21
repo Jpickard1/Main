cbf58e18e5831132859da7d88b7c00f3fa9ef21f
JPickard1 <jpic@umich.edu>
2023-06-20 20:34:45 -0400

    added a hardcoded optimization for 3-way tensors/hypergraphs for getting the counts tensor

--- Projects/AFOSR/kroneckerCalculations/HyperKronFit2/getCounts.m
+++ Projects/AFOSR/kroneckerCalculations/HyperKronFit2/getCounts.m
@@ -13,6 +13,13 @@ if ismatrix(theta)
         i2 = mod(floor((idxs(2) - 1)/ n0^(i-1)), n0) + 1;
         count(i1, i2) = count(i1, i2) + 1;
     end
+elseif ndims(theta) == 3
+    for i=1:kronExp
+        i1 = mod(floor((idxs(1) - 1)/ n0^(i-1)), n0) + 1;
+        i2 = mod(floor((idxs(2) - 1)/ n0^(i-1)), n0) + 1;
+        i3 = mod(floor((idxs(3) - 1)/ n0^(i-1)), n0) + 1;
+        count(i1, i2, i3) = count(i1, i2, i3) + 1;
+    end
 else
     kIdxs = zeros(kronExp, length(idxs));
     for i=1:length(idxs)
