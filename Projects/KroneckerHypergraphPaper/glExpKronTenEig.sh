0e28c28526cbb685de3f7540d0df8cf12777d14e
Joshua <jpic@umich.edu>
2023-07-12 16:53:34 -0400

    GL experiment

--- Projects/KroneckerHypergraphPaper/glExpKronTenEig.sh
+++ Projects/KroneckerHypergraphPaper/glExpKronTenEig.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=KTEig
+#SBATCH --job-name=KTEig9
 #SBATCH --mail-user=jpic@umich.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --mem=50GB
+#SBATCH --mem=25GB
 #SBATCH --time=3-00:00:00
 #SBATCH --account=indikar0
 #SBATCH --partition=standard
-#SBATCH --array=0-29
 #SBATCH --output=/nfs/turbo/umms-indikar/Joshua/Main/Projects/KroneckerHypergraphPaper/%x-%j.log
 
 
 module load matlab
-matlab -nodisplay -r "glEXpKronTenEig(2,2);"
+matlab -nodisplay -r "glExpKronTenEig(3,3);"
 
